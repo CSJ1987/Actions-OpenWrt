43f1b3110cf1e1e79efc206506a90890d768d5f9
Jason Chen <wxcsj1987@gmail.com>
2025-07-27 22:17:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@ sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/luci2/bin/config_genera
 # 添加netdata对ssl支持
 sed -i 's/+libjson-c/+libjson-c +libopenssl/g' feeds/packages/admin/netdata/Makefile
 sed -i 's/disable-https/enable-https/g' feeds/packages/admin/netdata/Makefile
-sed -i 's/http/https/g' feeds/luci/applications/luci-app-netdata/luasrc/view/netdata.htm
+#sed -i 's/http/https/g' feeds/luci/applications/luci-app-netdata/luasrc/view/netdata.htm
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
