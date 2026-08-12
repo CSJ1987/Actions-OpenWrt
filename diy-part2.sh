9ba60b1a95c7e9decae430184fbdec514a0cc357
Jason Chen <wxcsj1987@gmail.com>
2026-08-12 17:17:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@ sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/luci2/bin/config_generate
 # 添加netdata对ssl支持
 sed -i 's/+libjson-c/+libjson-c +libopenssl/g' feeds/packages/admin/netdata/Makefile
 sed -i 's/disable-https/enable-https/g' feeds/packages/admin/netdata/Makefile
-#sed -i 's/http/https/g' feeds/luci/applications/luci-app-netdata/luasrc/view/netdata.htm
+sed -i 's/http/https/g' feeds/luci/applications/luci-app-netdata/htdocs/luci-static/resources/view/netdata.js
 
 # UBOOT编译错误
 #sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
